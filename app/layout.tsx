492c03ac3b8c544812263b004b9098078451e3f7
Collins Ikechukwu <collinschristroa@gmail.com>
2024-10-30 11:15:30 +0100

    feat(layout): add Toaster component to RootLayout
    refactor(toaster): reorganize import statements in Toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
+import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'BSV',
-  description:
-    'BSV.'
+  description: 'BSV.'
 };
 
 export default function RootLayout({
@@ -15,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex min-h-screen w-full flex-col">{children}</body>
+      <body className="flex min-h-screen w-full flex-col">
+        {children}
+        <Toaster />
+      </body>
       <Analytics />
     </html>
   );
