f590f8cc11994991cf2d92ecd56f1e20a1e9058d
mo-jaber <mo.jaber@hotmail.com>
2024-10-31 13:09:41 +0400

    added dynamic = force-dynamic in the root layout
    added dynamic = force-dynamic in the root layout to opt out from prerendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
 
+export const dynamic = 'force-dynamic';
+
 export const metadata = {
   title: 'BSV',
   description: 'BSV.'
@@ -26,3 +28,4 @@ export default function RootLayout({
     </ClerkProvider>
   );
 }
+
