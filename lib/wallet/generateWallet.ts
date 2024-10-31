9c17af8ca9e6541f38fa357b8b6db0baac61b415
Collins Ikechukwu <collinschristroa@gmail.com>
2024-10-31 14:16:02 +0100

    feat(wallet): add generateWallet function to create new wallet with private and public keys

---
+++ lib/wallet/generateWallet.ts
@@ -0,0 +1,13 @@
+import { PrivateKey } from '@bsv/sdk';
+
+export function generateWallet() {
+  const privateKey = PrivateKey.fromRandom();
+  
+  const wif = privateKey.toWif();
+  
+  const publicKey = privateKey.toPublicKey();
+
+  const testnetAddress = publicKey.toAddress([0x6f]).toString();
+
+  return {wif, testnetAddress}
+}
