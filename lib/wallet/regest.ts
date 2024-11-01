3fd3406e915aec2076188d08a852f57451705853
Collins Ikechukwu <collinschristroa@gmail.com>
2024-11-01 15:11:50 +0100

    feat(wallet): add getRawTransaction method and fix typo in regest.ts

--- lib/wallet/regest.ts
+++ lib/wallet/regest.ts
@@ -1,4 +1,4 @@
-// src/utils/regtest.ts
+// src/utils/regest.ts
 import axios from 'axios';
 
 const TESTNET_API_URL = 'https://api.whatsonchain.com/v1/bsv/test';
@@ -9,6 +9,11 @@ export const getUTXOs = async (address: string) => {
 };
 
 export const broadcastTransaction = async (rawTx: string) => {
-  const response = await axios.post(`${TESTNET_API_URL}/tx/raw`, { rawtx: rawTx });
+  const response = await axios.post(`${TESTNET_API_URL}/tx/raw`, { txhex: rawTx });
+  return response.data;
+};
+
+export const getRawTransaction = async (tx_hash: string) => {
+  const response = await axios.get(`${TESTNET_API_URL}/tx/${tx_hash}/hex`);
   return response.data;
 };
