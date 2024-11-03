9057c49eb095e006bd91747a7399fc5597d28e8e
Ikem Peter <ikempeter2020@gmail.com>
2024-11-03 22:46:03 +0100

    add link to root path

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -6,12 +6,21 @@ import {
   CardTitle
 } from '@/components/ui/card';
 import DonationForm from '@/components/DonationForm'
+import { Button } from '@/components/ui/button';
+import { RefreshCcw } from 'lucide-react';
 
 export default function DashboardPage() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Dashboard</CardTitle>
+          <div className="flex w-full justify-between">
+            <CardTitle>Dashboard</CardTitle>
+            <Button variant="outline" onClick={() => {
+              window.location.href = '/donate';
+            }}>
+              <RefreshCcw className="w-4 h-4" /> Donate
+            </Button>
+          </div>
         <CardDescription>Dashboard items.</CardDescription>
       </CardHeader>
       <CardContent>
