95d820ec5d359756c5d5c4a6ab8767ee09e704db
saimeunt <saimeunt@gmail.com>
2024-11-04 13:56:23 +0100

    merge main

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,20 +1,28 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle
-  } from '@/components/ui/card';
-  
-  export default function DashboardPage() {
-    return (
-      <Card>
-        <CardHeader>
-          <CardTitle>Dashboard</CardTitle>
-          <CardDescription>Dashboard items.</CardDescription>
-        </CardHeader>
-        <CardContent></CardContent>
-      </Card>
-    );
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+import { fetchUser, fetchTransactions } from '@/lib/prisma';
+import LatestTransactionsTable from '../latest-transactions-table';
+
+export default async function DashboardPage() {
+  const user = await fetchUser();
+  if (!user) {
+    return null;
   }
-  
+  const transactions = await fetchTransactions(user, 5);
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Dashboard</CardTitle>
+        <CardDescription>Dashboard items.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <LatestTransactionsTable user={user} transactions={transactions} />
+      </CardContent>
+    </Card>
+  );
+}
---
+++ app/(dashboard)/latest-transactions-table.tsx
@@ -0,0 +1,42 @@
+import { User, TransactionWithUser } from '@/lib/prisma';
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table';
+import LatestTransactionsTableRow from './latest-transactions-table-row';
+
+const LatestTransactionsTable = ({
+  user,
+  transactions
+}: {
+  user: User;
+  transactions: TransactionWithUser[];
+}) => (
+  <Table className="overflow-x-auto">
+    <TableHeader>
+      <TableRow>
+        <TableHead>Tx ID</TableHead>
+        <TableHead>Date</TableHead>
+        {user.role === 'admin' && <TableHead>Account</TableHead>}
+        <TableHead>Beef Tx</TableHead>
+        <TableHead>Tx Type</TableHead>
+        <TableHead>Amount</TableHead>
+        <TableHead>Status</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {transactions.map((transaction) => (
+        <LatestTransactionsTableRow
+          key={transaction.txid}
+          user={user}
+          transaction={transaction}
+        />
+      ))}
+    </TableBody>
+  </Table>
+);
+
+export default LatestTransactionsTable;
