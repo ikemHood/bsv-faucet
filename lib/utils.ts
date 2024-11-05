ef85076e7dda4fa71885ae4848c26f8d8d67c1a1
Ikem Peter <ikempeter2020@gmail.com>
2024-11-05 19:36:18 +0100

    re/feat/donate token back to faucet

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const AdminWalletAddress = process.env.NEXT_PUBLIC_ADMIN_WALLET_ADDRESS || 'mgqipciCS56nCYSjB1vTcDGskN82yxfo1G';
