67b7a7f723124ae074af534e5a9288b2fec97ebf
Ikem Peter <ikempeter2020@gmail.com>
2024-11-05 15:39:52 +0100

    removed donate btn on home route

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -7,24 +7,15 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'; 
-import { Button } from '@/components/ui/button';
-import { RefreshCcw } from 'lucide-react';
 
 export default function DashboardPage() {
   return (
     <Card>
       <CardHeader>
-          <div className="flex w-full justify-between">
-            <CardTitle>Dashboard</CardTitle>
-            <Button variant="outline" onClick={() => {
-              window.location.href = '/donate';
-            }}>
-              <RefreshCcw className="w-4 h-4" /> Donate
-            </Button>
-          </div>
+        <CardTitle>Dashboard</CardTitle>
         <CardDescription>Dashboard items.</CardDescription>
-      </CardHeader><CardContent>
-      </CardContent>
+      </CardHeader>
+      <CardContent></CardContent>
     </Card>
   );
 }
